38737eeeb169319d08d026472cadd337d2cad357
Bilal Ahmad <bilalmomand709@gmail.com>
2024-09-05 17:31:04 +0500

    practiced about basic things

---
+++ my-first-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Starting with the glorious name of Allah Almighty the most merciful the most beneficent </h3>
+      <br/>
+      <h3>In today's practice I learned about:</h3>
+      <ol>
+        <li>What is React js</li>
+        <li>Folders Structure</li>
+        <li>Work Flow</li>
+        <li>Many more</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
